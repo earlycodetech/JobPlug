67939b4e3ba20243efd042d9a4111a2ff7b79498
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-01-08 13:05:35 +0100

    -M

--- Framework/Navigators/StackNavigator.js
+++ Framework/Navigators/StackNavigator.js
@@ -1,7 +1,9 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { HomeScreen } from "../Screens/HomeScreen";
 import { Intro } from "../Screens/Intro";
+import { HomeScreen } from "../Screens/HomeScreen";
+import { Login } from "../Screens/Login";
+import { Signup } from "../Screens/Signup";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,8 +11,10 @@ export function StackNavigator() {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Intro" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="HomeScreen" component={HomeScreen} />
                 <Stack.Screen name="Intro" component={Intro} />
+                <Stack.Screen name="HomeScreen" component={HomeScreen} />
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Signup" component={Signup} />
             </Stack.Navigator>
         </NavigationContainer>
     );
